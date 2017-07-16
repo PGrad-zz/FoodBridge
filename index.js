5e7f22093296ae94ed375b4cb160de3f94c6726e
Glyne Gittens <jgit121@gmail.com>
2017-07-15 19:26:19 -0700

    Database connects correctly, db instance is tied to the request object. Added to project structure. Please change all html file extensions to ejs

--- index.js
+++ index.js
@@ -1,45 +1,88 @@
-var server = require("./server");
-var router = require("./router");
-var requestHandlers = require("./requestHandlers");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var MongoClient = require("mongodb").MongoClient;
 var mongo_config = require("./config/mongodb_config")
 var fs = require("fs");
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-var handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
-handle["/show"] = requestHandlers.show;
-handle["/home"] = requestHandlers.home;
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, ""));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 fs.stat("./mongodb_config_local.json", function(fs_err, stats){
 	if(fs_err == null){
 		console.log("Using a local route"); 
-		MongoClient.connect("mongodb://localhost:27017/foodbridge", function(err, db){
+		MongoClient.connect("mongodb://localhost:27017/foodbridge", function(err, database){
 			if(err == null){
+				app.use(function(req, res, next){
+					req.db = database;
+					console.log(req.db);
+					next();
+				})
+				express.request.db = database;
+
 				console.log("WORKING!");
-				server.start(router.route, handle);
+				app.listen(8888);
 			}
 			else {
 				console.log(err);
 			}
-			db.close();
 		})
 		
 	} else{
 		console.log("joining remote server");
-		MongoClient.connect(mongo_config.uri, function(err, db){
+		MongoClient.connect(mongo_config.uri, function(err, database){
 			if(err == null){
-				server.start(router.route, handle);
+				app.use(function(req, res, next){
+					req.db = database;
+					console.log(req.db);
+					next();
+				})
+				app.listen(8888);
 			}
 			else {
 				console.log(err);
 			}
-			db.close();
+
 		})
 	}
 })
 
-
-
+module.exports = app;
