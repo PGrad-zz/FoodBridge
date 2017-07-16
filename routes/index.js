a37e632a073517af6bc40f873889467f0f5687c7
Glyne Gittens <jgit121@gmail.com>
2017-07-16 03:04:18 -0700

    Fixed Login

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,15 @@ router.get('/', function(req, res, next) {
 
 router.post('/donor/login', function(req, res){
 	var accnt = req.db.collection("account");
-	accnt.find({email: req.body.email}, function(err, record){
+	accnt.find({email: req.body.email}).toArray(function(err, record){
 		if(err){
 			console.log("couldn't find record, redirecting");
 			res.redirect("../");
 		}
 		else{
-			if(record.password == req.body.password){
+			console.log(record[0].password);
+			console.log(req.body.password);
+			if(record[0].password == req.body.password){
 				console.log("password correct");
 				res.redirect("../posts");	
 			}
@@ -30,20 +32,23 @@ router.post('/donor/login', function(req, res){
 
 router.post('/charity/login', function(req, res){
 	var accnt = req.db.collection("account");
-	accnt.find({email: req.body.email} , function(err, record){
+	accnt.find({email: req.body.email}).toArray(function(err, record){
 		if(err){
 			console.log("couldn't find record, redirecting");
 			res.redirect("../");
 		}
 		else{
-			if(record.password == req.body.password){
+			console.log(record[0].password);
+			console.log(req.body.password);
+			if(record[0].password == req.body.password){
 				console.log("password correct");
 				res.redirect("../posts");	
 			}
 			else{
 				console.log("password wrong");
 				res.redirect("../");
-			}			
+			}
+			
 		}
 	})
 });
@@ -128,6 +133,7 @@ router.post('/post/new', function(req, res, next){
 		claimant: "none",
 		expired: false,
 		phone: req.body.phone,
+		status: "open",
 		created: new Date()
 	};
 	console.log(newPost);
@@ -136,6 +142,10 @@ router.post('/post/new', function(req, res, next){
 	});
 	res.redirect('../posts');
 });
+
+router.post('/post/update', function(req, res){
+
+})
 
 router.get('/donor', function(req, res, next) {
 	console.log(req.db.collection);
