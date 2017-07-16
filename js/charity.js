2d133b5bca77bd9b02c2a5e7761c370b973ec117
Adrian Garza <adrianhgarza@gmail.com>
2017-07-15 20:33:34 -0700

    updated js loading of posts

--- js/charity.js
+++ js/charity.js
@@ -1,4 +1,4 @@
-function fadeIn(i, elements, duration, callback){
+/*function fadeIn(i, elements, duration, callback){
             if(i >= elements.length)
                 $.isFunction(callback) && callback();
             else
@@ -8,3 +8,31 @@ function fadeIn(i, elements, duration, callback){
         }
 
 fadeIn(0, $('ul li'), 250)
+*/
+$(document).ready(function() {
+	var testBlob = "                    <li>\
+                        <div class=\"panel panel-default posting\">\
+                            <div class=\"panel-body\">\
+                                <span class='col-md-8'>\
+                                    <p class='post-name'>Burritos and Tamales</p>\
+                                    <p class='post-donor'> Zynga </p>\
+                                    <p>40 half burritos</p>\
+                                    <div class='post-description'>about 2/3 of them are vegetarian and...</div>\
+                                </span>\
+                                <span class='col-md-4'>\
+                                    <br>\
+                                     <p class='post-contact'> Contact at: </p>\
+                                     <br>\
+                                </span>\
+                            </div>\
+                        </div>\
+                    </li>"
+    var added = 0;
+    var tempInterval = setInterval(function() {
+    	$("#post-container").append(testBlob);
+    	added++;
+    	if (added > 10) {
+    		clearInterval(tempInterval);
+    	}
+    }, 50)
+})
