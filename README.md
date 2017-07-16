38d3dd0cfc514e3040b5fb1c0f8d8d9c0bfedd69
Glyne Gittens <jgit121@gmail.com>
2017-07-15 17:15:24 -0700

    Added mongodb connections and changed README to point out configurations

--- README.md
+++ README.md
@@ -3,10 +3,26 @@ FoodBridge app repo for Hack4Diversity
 
 To Run the app:
 
-- Run MongoDb
+- Run MongoDb locally
+	Create a folder called data/db/
+	```
+		mkdir -p ./data/db
+
+	``` 
+		*mongod --dbpath ./data/db --port 27017* 
+
+	Create a file called *mongodb_config_local.json* with this text:
+	```	
+		{
+			"config": {
+				"url": "mongodb://localhost:27017/foodbridge"
+			}
+		}
 
 - Make sure you have nodemon installed:
-	``` *npm install -g nodemon*
+	``` 
+		*npm install -g nodemon*
 
 - Then run from the commandline:
-	``` *npm run app*
+	``` 
+		*npm run app*
--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
+var MongoClient = require("mongodb").MongoClient;
+var mongo_config = require("./config/mongodb_config")
+var fs = require("fs");
+
 
 var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
 handle["/show"] = requestHandlers.show;
+handle["/home"] = requestHandlers.home;
 
-server.start(router.route, handle);
+fs.stat("./mongodb_config_local.json", function(fs_err, stats){
+	if(fs_err == null){
+		console.log("Using a local route"); 
+		MongoClient.connect("mongodb://localhost:27017/foodbridge", function(err, db){
+			if(err == null){
+				console.log("WORKING!");
+				server.start(router.route, handle);
+			}
+			else {
+				console.log(err);
+			}
+			db.close();
+		})
+		
+	} else{
+		console.log("joining remote server");
+		MongoClient.connect(mongo_config.uri, function(err, db){
+			if(err == null){
+				server.start(router.route, handle);
+			}
+			else {
+				console.log(err);
+			}
+			db.close();
+		})
+	}
+})
+
+
+
