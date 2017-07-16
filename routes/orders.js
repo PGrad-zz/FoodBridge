802a377e50d5440e326392aa98fc4485e646f46d
Glyne Gittens <jgit121@gmail.com>
2017-07-15 22:46:39 -0700

    Changed charity.ejs to posts.ejs. Added some routing logic for posts and new posts

--- routes/orders.js
+++ routes/orders.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   var db = req.db;
   var orders = db.collection('order');
   var results = orders.find();  
   res.render('index', results);
+});
+
+router.post('/', function(req, res) {
+	var db = req.db;
+	var orders = db.collection('order');
+	var results = order.insert();
 });
 
 module.exports = router;
